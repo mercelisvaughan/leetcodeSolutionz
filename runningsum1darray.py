0c2729ac254bca8ad928e72766832950d0bb7189
Mercelis Vaughan <mercelisvaughan@gmail.com>
2025-06-25 13:38:59 -0400

    added runningsum1darray

---
+++ runningsum1darray.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        runningSum = 0 # sets the count to zero
+        for i in nums:
+            runningSum += nums[i]
+            nums[i] = runningSum
+        return nums
+    
