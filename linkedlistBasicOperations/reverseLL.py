54d94f79ba834dd86131f372afa3ae9f5e9b1cc8
Mercelis Vaughan <mercelisvaughan@gmail.com>
2025-10-21 13:01:13 -0400

    added reverse a linked list

---
+++ linkedlistBasicOperations/reverseLL.py
@@ -0,0 +1,22 @@
+
+
+
+class SinglyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def reverseLL(head):
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next
+
+            curr.next = prev
+
+            prev = curr
+            curr = nxt
+            
+        return prev
